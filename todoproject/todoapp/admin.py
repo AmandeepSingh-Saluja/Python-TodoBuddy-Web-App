8702b47ebd3dc32a36bcced3e0772fe993552df0
AmanSingh <amandeepsalujaa@gmail.com>
2025-04-14 23:01:57 +0530

    FILE ADDED

---
+++ todoproject/todoapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import Task
+
+# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'priority', 'created_at', 'due_date')
+    list_filter = ('status', 'priority')
+    search_fields = ('title',)
